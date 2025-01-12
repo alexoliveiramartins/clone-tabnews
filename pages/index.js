0d2ca2bab613b8620ec351e327f9876938a31a8b
Alex Benjamim de Oliveira Martins <155112179+alexoliveiramartins@users.noreply.github.com>
2025-01-12 00:59:31 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Quem diz que não pode ser feito nunca deve interromper aquele que está fazendo. - Monkey D. Luffy 😎</h1>
+function Home() {
+  return (
+    <h1>
+      Quem diz que não pode ser feito nunca deve interromper aquele que está
+      fazendo. - Monkey D. Luffy 😎
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
